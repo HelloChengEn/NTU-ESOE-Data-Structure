0cf0dfb525744ed7aa1d59de65b432ac7c0d3cca
HelloChengEn <smiletsai871017@gmail.com>
2022-09-27 18:50:04 +0800

    ca1prob1

--- ca1/CA1Prob1/CA1Prob1.cpp
+++ ca1/CA1Prob1/CA1Prob1.cpp
@@ -1,15 +1,46 @@
-﻿// CA1Prob1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
+﻿#include <iostream>
 
-#include <iostream>
+int dotProduct(int a[], int b[], int n);
+void bubbleSort(int a[], int n);
+using namespace std;
 
 int main()
 {
-    int a;
-    std::cin >> a;
-    std::cout << a << "Hello World!\n";
+    int a[] = {-1,0,2,15,7,6,-4,8,21,-13 };
+    int b[] = { 5,9,-18,16,0,1,-4,18,-2,12 };
+    int n = sizeof(a) / sizeof(a[0]);
+
+    // sort array
+    bubbleSort(a, n);
+    bubbleSort(b, n);
+
+    // compute dot product
+     int res = dotProduct(a, b, n);
+     cout << res;
 }
 
+int dotProduct(int a[], int b[], int n)
+{
+    int res = 0;
 
+    for (int i = 0; i < n; n++) {
+        res += a[i] * b[i];
+    }
+
+    return res;
+}
+
+void bubbleSort(int a[], int n)
+{
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j+1] = temp;
+            }
+        }
+    }
+}
 
 
