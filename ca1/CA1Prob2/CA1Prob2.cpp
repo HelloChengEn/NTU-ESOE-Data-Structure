e53c4ed037edb1dbceb838ca73521e44b9ebd16b
HelloChengEn <smiletsai871017@gmail.com>
2022-09-28 20:42:32 +0800

    update

--- ca1/CA1Prob2/CA1Prob2.cpp
+++ ca1/CA1Prob2/CA1Prob2.cpp
@@ -1,11 +1,12 @@
-﻿// CA1Prob2.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
+﻿#include <iostream>
 
-#include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    cout << "Hello World!\n";
 }
 
 
